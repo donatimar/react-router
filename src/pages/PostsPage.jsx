c30196d1453d99a94493080502dbe0eb2b0167af
donatimar <donatimar@gmail.com>
2024-12-10 14:10:48 +0100

    added ul to postsPage

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,13 @@
-export default function HomePage() {
+export default function PostsPage() {
   return (
     <div className="container py-5">
       <h1>Lista dei post</h1>
+      <ul>
+        <li>Post 1: Lorem</li>
+        <li>Post 2: Ipsum</li>
+        <li>Post 3: Dolor</li>
+        <li>Post 4: Sit</li>
+      </ul>
     </div>
   );
 }
